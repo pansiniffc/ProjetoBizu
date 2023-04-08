df198a41f8b0f9b9c81944bf9c709ce6c23a8516
Renan Pansini <renanpansini15@gmail.com>
2023-04-08 16:25:47 -0400

    react route add

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import '../styles/App.css';
 import Layout from './Layout';
-// import Home from './pages/Home';
-// import Login from './pages/Login';
-// import Signup from './pages/Signup';
-// import Quiz from './pages/Quiz';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Quiz from './pages/Quiz';
 import Result from './pages/Result';
 
 function App() {
   return (
-    <Layout>
-      {/* <Home /> */}
-      {/* <Signup /> */}
-      {/* <Login /> */}
-      {/* <Quiz /> */}
-      <Result />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
